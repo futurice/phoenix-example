0adfe7d3e9551266f3f8c81d8ceb777f1e722492
Martin Richter <martin.richter@futurice.com>
2017-01-05 17:29:05 +0100

    List posts at site root

--- web/router.ex
+++ web/router.ex
@@ -16,8 +16,7 @@ defmodule Blog.Router do
   scope "/", Blog do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
-    resources "/posts", PostController, only: [:index]
+    get "/", PostController, :index
   end
 
   # Other scopes may use custom stacks.
