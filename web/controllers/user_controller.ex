9d5a1f307d465d37714faa9d78319c92a6da3937
Martin Richter <me@martinrichter.net>
2017-01-17 23:35:41 +0100

    Show validation errors in user form

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -16,10 +16,15 @@ defmodule Blog.UserController do
 
   def create(conn, %{"user" => user_params}) do
     changeset = User.changeset(%User{}, user_params)
-    {:ok, user} = Repo.insert(changeset)
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
 
-    conn
-    |> put_flash(:info, "#{user.name} created!")
-    |> redirect(to: user_path(conn, :index))
+
   end
 end
