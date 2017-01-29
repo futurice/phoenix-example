7643539b8b50926f5df9b8aa60b38a1d9f3a0f35
Martin Richter <martin.richter@futurice.com>
2017-01-29 22:10:05 +0200

    Test that post index only shows user's posts

--- test/controllers/post_controller_test.exs
+++ test/controllers/post_controller_test.exs
@@ -1,6 +1,16 @@
 defmodule Blog.PostControllerTest do
   use Blog.ConnCase
 
+  setup %{conn: conn} = config do
+    if username = config[:login_as] do
+      user = insert_user(username: username)
+      conn = assign(build_conn(), :current_user, user)
+      {:ok, conn: conn, user: user}
+    else
+      :ok
+    end
+  end
+
   test "requires user authentication on all actions", %{conn: conn} do
     Enum.each([
       get(conn, post_path(conn, :new)),
@@ -14,5 +24,17 @@ defmodule Blog.PostControllerTest do
       assert html_response(conn, 302)
       assert conn.halted
     end)
+  end
+
+  @tag login_as: "max"
+  test "lists all the user's posts on index", %{conn: conn, user: user} do
+    user_post = insert_post(user, title: "User post", body: "")
+    other_post = insert_post(insert_user(username: "other"),
+      title: "Other post", body: "")
+
+    conn = get conn, post_path(conn, :index)
+    assert html_response(conn, 200) =~ ~r/Listing posts/
+    assert String.contains?(conn.resp_body, user_post.title)
+    refute String.contains?(conn.resp_body, other_post.title)
   end
 end
--- test/support/test_helpers.ex
+++ test/support/test_helpers.ex
@@ -2,7 +2,7 @@ defmodule Blog.TestHelpers do
   alias Blog.Repo
 
   def insert_user(attrs \\ %{}) do
-    changes = Map.merge(%{
+    changes = Dict.merge(%{
       name: "Some user",
       username: "user#{Base.encode16(:crypto.strong_rand_bytes(8))}",
       password: "supersecret",
