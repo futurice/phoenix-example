1a622d50c7debc6fd06425b682e065e578819081
Martin Richter <martin.richter@futurice.com>
2017-01-29 21:46:13 +0200

    Start rewriting post integration tests
    This replaces the existing, broken generated tests with one that
    tests auth redirection for a start.

--- test/controllers/post_controller_test.exs
+++ test/controllers/post_controller_test.exs
@@ -1,67 +1,18 @@
 defmodule Blog.PostControllerTest do
   use Blog.ConnCase
 
-  alias Blog.Post
-  @valid_attrs %{body: "Body", title: "Title"}
-  @valid_post %Post{body: "Body", title: "Title"}
-  @invalid_attrs %{}
-
-  test "lists all entries on index", %{conn: conn} do
-    conn = get conn, post_path(conn, :index)
-    assert html_response(conn, 200) =~ "Listing posts"
-  end
-
-  test "renders form for new resources", %{conn: conn} do
-    conn = get conn, post_path(conn, :new)
-    assert html_response(conn, 200) =~ "New post"
-  end
-
-  test "creates resource and redirects when data is valid", %{conn: conn} do
-    conn = post conn, post_path(conn, :create), post: @valid_attrs
-    assert redirected_to(conn) == post_path(conn, :index)
-    assert Repo.get_by(Post, @valid_attrs)
-  end
-
-  test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, post_path(conn, :create), post: @invalid_attrs
-    assert html_response(conn, 200) =~ "New post"
-  end
-
-  test "shows chosen resource", %{conn: conn} do
-    post = Repo.insert! @valid_post
-    conn = get conn, post_path(conn, :show, post)
-    assert html_response(conn, 200) =~ "Show post"
-  end
-
-  test "renders page not found when id is nonexistent", %{conn: conn} do
-    assert_error_sent 404, fn ->
-      get conn, post_path(conn, :show, -1)
-    end
-  end
-
-  test "renders form for editing chosen resource", %{conn: conn} do
-    post = Repo.insert! @valid_post
-    conn = get conn, post_path(conn, :edit, post)
-    assert html_response(conn, 200) =~ "Edit post"
-  end
-
-  test "updates chosen resource and redirects when data is valid", %{conn: conn} do
-    post = Repo.insert! @valid_post
-    conn = put conn, post_path(conn, :update, post), post: @valid_attrs
-    assert redirected_to(conn) == post_path(conn, :show, post)
-    assert Repo.get_by(Post, @valid_attrs)
-  end
-
-  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-    post = Repo.insert! @valid_post
-    conn = put conn, post_path(conn, :update, post), post: @invalid_attrs
-    assert html_response(conn, 200) =~ "Edit post"
-  end
-
-  test "deletes chosen resource", %{conn: conn} do
-    post = Repo.insert! @valid_post
-    conn = delete conn, post_path(conn, :delete, post)
-    assert redirected_to(conn) == post_path(conn, :index)
-    refute Repo.get(Post, post.id)
+  test "requires user authentication on all actions", %{conn: conn} do
+    Enum.each([
+      get(conn, post_path(conn, :new)),
+      get(conn, post_path(conn, :index)),
+      get(conn, post_path(conn, :show, "123")),
+      get(conn, post_path(conn, :edit, "123")),
+      get(conn, post_path(conn, :update, "123", %{})),
+      get(conn, post_path(conn, :create, %{})),
+      get(conn, post_path(conn, :delete, "123")),
+    ], fn conn ->
+      assert html_response(conn, 302)
+      assert conn.halted
+    end)
   end
 end
