38eee2a9ed48c1833f9b99242b5ba19ee2de8cc2
Martin Richter <martin.richter@futurice.com>
2017-02-19 10:39:57 +0100

    Generate slugs for posts and use it in URLs

--- web/models/post.ex
+++ web/models/post.ex
@@ -1,9 +1,11 @@
 defmodule Blog.Post do
   use Blog.Web, :model
 
+  @primary_key {:id, Blog.Permalink, autogenerate: true}
   schema "posts" do
     field :title, :string
     field :body, :string
+    field :slug, :string
     belongs_to :author, Blog.User
     belongs_to :category, Blog.Category
 
@@ -17,6 +19,27 @@ defmodule Blog.Post do
     struct
     |> cast(params, [:title, :body, :category_id])
     |> validate_required([:title, :body])
+    |> slugify_title()
     |> assoc_constraint(:category)
+  end
+
+  defimpl Phoenix.Param, for: Blog.Post do
+    def to_param(%{slug: slug, id: id}) do
+      "#{id}-#{slug}"
+    end
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
   end
 end
