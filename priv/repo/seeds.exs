36cad335817fce7b18759c89e4798a298db49cf7
Martin Richter <me@martinrichter.net>
2017-03-27 16:07:13 +0200

    Seed some users needed to try the service

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -11,7 +11,16 @@
 # and so on) as they will fail if something goes wrong.
 
 alias Blog.Repo
+alias Blog.User
 alias Blog.Category
+
+# Create demo user
+Repo.insert!(User.registration_changeset(%User{},
+  %{name: "Demo User", username: "demo", password: "secret"}))
+
+# Create Wolfram Alpha bot
+Repo.insert!(User.registration_changeset(%User{},
+  %{name: "Wolfram Alpha", username: "wolfram", password: "wolfram"}))
 
 for category <- ["Coding", "World Domination", "Testing"] do
   Repo.get_by(Category, name: category) ||
